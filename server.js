29c33f9db0179a76f333b6b67a8a702533de3192
chhour.thaisan <thaisan@tabnext.asia>
2023-05-29 16:50:10 +0700

    json-server

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+// const jsonServer = require('json-server')
+// const cors = require('cors')
+// const path = require('path')
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+// const server = jsonServer.create()
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults()
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+// server.use(cors())
+// server.use(jsonServer.bodyParser)
+// server.use(middlewares)
+// server.use(router)
 
-const PORT = 3000
+// const PORT = 3000
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on http://localhost:${PORT}`)
+// })
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
